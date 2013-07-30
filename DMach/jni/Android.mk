c3633177e8114891e48eca41f0156e9f1a10f549
Simon Norberg <simon.norberg@gmail.com>
2013-07-30 22:42:10 +0200

    Removed mips target and fixed ndk r9 compilation errors.

--- DMach/jni/Android.mk
+++ DMach/jni/Android.mk
@@ -1,49 +1,44 @@
 LOCAL_PATH := $(call my-dir)
 
+PD_C_INCLUDES := $(LOCAL_PATH)/../../../../pd-for-android/PdCore/jni/libpd/pure-data/src
+
 #---------------------------------------------------------------
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := shared
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../pd-for-android/PdCore/jni/libpd/pure-data/src
+LOCAL_C_INCLUDES := $(PD_C_INCLUDES)
 LOCAL_CFLAGS := -DPD
-LOCAL_LDFLAGS := -WI,--export-dynamic
 LOCAL_SRC_FILES := shared.c
-LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../../../pd-for-android/PdCore/libs/$(TARGET_ARCH_ABI) -lpd
 include $(BUILD_STATIC_LIBRARY)
 
 #---------------------------------------------------------------
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := loud
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../pd-for-android/PdCore/jni/libpd/pure-data/src
+LOCAL_C_INCLUDES := $(PD_C_INCLUDES)
 LOCAL_CFLAGS := -DPD
-LOCAL_LDFLAGS := -WI,--export-dynamic
 LOCAL_SRC_FILES := loud.c
-LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../../../pd-for-android/PdCore/libs/$(TARGET_ARCH_ABI) -lpd
 include $(BUILD_STATIC_LIBRARY)
 
 #---------------------------------------------------------------
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := sic
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../pd-for-android/PdCore/jni/libpd/pure-data/src
+LOCAL_C_INCLUDES := $(PD_C_INCLUDES)
 LOCAL_CFLAGS := -DPD
-LOCAL_LDFLAGS := -WI,--export-dynamic
 LOCAL_SRC_FILES := sic.c
-LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../../../pd-for-android/PdCore/libs/$(TARGET_ARCH_ABI) -lpd
-LOCAL_STATIC_LIBRARIES := shared loud include
+LOCAL_STATIC_LIBRARIES := shared loud
 include $(BUILD_STATIC_LIBRARY)
 
 #---------------------------------------------------------------
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := tanh_tilde
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../pd-for-android/PdCore/jni/libpd/pure-data/src
+LOCAL_C_INCLUDES := $(PD_C_INCLUDES)
 LOCAL_CFLAGS := -DPD
-LOCAL_LDFLAGS := -WI,--export-dynamic
 LOCAL_SRC_FILES := tanh.c
 LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../../../pd-for-android/PdCore/libs/$(TARGET_ARCH_ABI) -lpd
-LOCAL_STATIC_LIBRARIES := sic include
+LOCAL_STATIC_LIBRARIES := sic
 include $(BUILD_SHARED_LIBRARY)
 
 #---------------------------------------------------------------
--- DMach/jni/Application.mk
+++ DMach/jni/Application.mk
@@ -1,2 +1,3 @@
 APP_OPTIM := release
-APP_ABI := all
+APP_ABI := armeabi armeabi-v7a x86
+NDK_TOOLCHAIN_VERSION=4.8
